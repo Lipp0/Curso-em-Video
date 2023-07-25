f308942da25fb2001cbd890dc20c9bb8be959775
Michel Filippo <81443283+Lipp0@users.noreply.github.com>
2023-07-24 22:15:42 -0300

    .

---
+++ Python/Mundo 1/Aulas/aula07a.py
@@ -0,0 +1,23 @@
+# Operadores Aritméticos:
+
+# + = adição (5+2=7)
+# - = subtração (5-2=3)
+# * = multiplicação (5*2=10)
+# / = divisão (5/2=2.5)
+# ** = exponenciação (5**2=25)
+# // = divisão inteira (5//2=2)
+# % = resto da divisão (5%2=1)
+
+# Ordem de precedência:
+
+# 1°- parênteses;
+# 2°- exponenciação;
+# 3°- multiplicação, divisão, divisão inteira e resto da divisão (não tem uma ordem definida, fazer o que aparecer primeiro);
+# 4°- adição e subtração.
+
+# Exemplos:
+
+# 5+3*2 => 5+6 => 11
+# 3*5+4**2 => 3*5+16 => 15+16 => 31
+# 3*(5+4)**2 => 3*9**2 => 3*81 => 243
+
