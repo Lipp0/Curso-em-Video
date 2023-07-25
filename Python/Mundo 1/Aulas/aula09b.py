e7d08e2d173575bbf290097d5acfda27f02ac4fe
Michel Filippo <81443283+Lipp0@users.noreply.github.com>
2023-07-24 22:33:52 -0300

    .

---
+++ Python/Mundo 1/Aulas/aula09b.py
@@ -0,0 +1,25 @@
+frase = 'Curso em Vídeo Python'
+print('-'*20)
+print(frase[3])
+print(frase[3:13])
+print(frase[:13])
+print(frase[13:])
+print(frase[1:15:2])
+print(frase[::2])
+print(frase.count('o'))
+print(frase.upper().count(o))
+print(len(frase))
+print(len(frase.stip()))
+print(frase.find('Curso'))
+print(frase.find(vídeo))
+dividido = frase.split()
+print(dividido[2][3])
+frase = frase.replace('Python', 'Android')
+print(frase)
+print('-'*20)
+print("""Welcome! Are you completely new to programming?
+about why and how to get started with Python. Fortnunately
+an experienced programmer in any programming language
+(whatever it may be) can íck up Python very quickly.
+Its also easy for beginners to use and learn, so jump in!""")
+
