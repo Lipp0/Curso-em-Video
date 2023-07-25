6f6ff151d1d202300a204fa00512e40326297afe
Michel Filippo <81443283+Lipp0@users.noreply.github.com>
2023-07-24 22:14:42 -0300

    .

---
+++ Python/Mundo 1/Aulas/aula06b.py
@@ -0,0 +1,11 @@
+# testes de tipo:
+# print(x.isnumeric()) mostra se o valor 'x' é um número
+# print(x.isalpha()) mostra se o 'x' é letra
+# print(x.isalnum()) mostra se o 'x' é alfanumérico
+# print(x.isupper()) mostra se o 'x' está somente em letra maiúscula
+
+
+n = float(input("Digite um valor: "))
+print(n)
+n1 = bool(input("Digite um valor: "))
+print(n1)
